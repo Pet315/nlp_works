e295814a75c49351228194744a5a2a02b8bf563c
Pet315 <oppapeter17@gmail.com>
2022-11-27 18:01:38 +0200

    Add changes

--- kyivbot/Element.py
+++ kyivbot/Element.py
@@ -5,13 +5,10 @@ from kyivbot.View import View
 
 
 class Element:
-    def __init__(self, element, file_name, column_name, additional_element=None,
-                       additional_element_cn=None, first_row_number=2):
+    def __init__(self, element, file_name, column_name='A', first_row_number=2):
         self.element = element
         self.file_name = file_name
         self.column_name = column_name
-        self.additional_element = additional_element
-        self.additional_element_cn = additional_element_cn  # additional_element_column_name
         self.first_row_number = first_row_number
 
     def use_excel_data(self, user_element):
@@ -20,42 +17,71 @@ class Element:
         workbook.active = 0
         excel_data = workbook.active
         i = self.first_row_number
+        ed_elements = []
         while 1:
             if excel_data[self.column_name + str(i)].value is None:
-                return ' не знайдено'
+                if len(ed_elements) == 0:
+                    return [' не знайдено']
+                else:
+                    ed_elements.append(excel_data[self.column_name + '1'].value)
+                    return ed_elements
             ed_element = str(excel_data[self.column_name + str(i)].value)  # excel_data_element
             ed_element_lower = ed_element.lower()  # excel_data_element_lower
-            if self.additional_element is not None:
-                if re.search(self.additional_element, excel_data[self.additional_element_cn + str(i)].value):
-                    i += 1
-                    continue
+
             if re.search(user_element, ed_element_lower):
-                return ed_element
+                ed_elements.append(ed_element)
             i += 1
 
-    def define_element(self):
+    def define_param(self):
         while 1:
-            if self.element.find(' ') != -1: # Берег Дніпра => берег Дніпра
+            if self.element.find(' ') != -1:  # Берег Дніпра => берег Дніпра
                 element = self.element.split(' ')
                 element = element[0].lower() + ' ' + element[1]
-            else: # Район => район
+            else:  # Район => район
                 element = self.element.lower()
             View.output('Введіть ' + element + ': ')
             user_element = input()
-            found_element = self.use_excel_data(user_element)
+            found_element = self.use_excel_data(user_element)[0]
+
             if user_element != found_element:
                 if found_element == ' не знайдено':
-                    View.output(self.element + found_element) # EDIT!!!
+                    View.output(self.element + found_element)
                 else:
                     View.output('Ви мали на увазі: ' + found_element + "? ")
                     district_check = input()
                     if re.search('так', district_check):
-                        View.output(self.element + ' зафіксовано')  # EDIT!!!
+                        View.output(self.element + ' зафіксовано')
                         return found_element
                 View.output('Бажаєте спробувати ще раз?')
                 district_check = input()
                 if re.search('так', district_check):
                     continue
                 return ' не знайдено'
-            View.output(self.element + ' зафіксовано')  # EDIT!!!
+            View.output(self.element + ' зафіксовано')
             return found_element
+
+    def define_obj_nearby(self, user_element, file_name, column_name='A'):
+        self.file_name = file_name
+        self.column_name = column_name
+        user_element_list = user_element.split(' ')
+        user_element = ''
+
+        for i in range(len(user_element_list) - 1):
+            user_element += user_element_list[i]
+            if i != 0:
+                user_element += ' '
+        found_elements = self.use_excel_data(user_element)
+
+        if found_elements[0] == ' не знайдено':
+            # "Продуктового магазину за вулицею не знайдено" => LEMMA+POS!!!
+            View.output(self.element + found_elements[0])
+        found_elements_str = ''
+
+        for i in range(len(found_elements)-1):
+            found_element_list = found_elements[i].split(' ')
+            found_element = ''
+            for i in range(1, len(found_element_list)):
+                found_element += found_element_list[i]
+                found_element += ' '
+            found_elements_str += found_elements[len(found_elements)-1] + ' - ' + found_element + '\n'
+        return found_elements_str
--- kyivbot/main.py
+++ kyivbot/main.py
@@ -18,26 +18,20 @@ def main():
             program.output(messages.params)
             way = program.input(['1', 'один', '2', 'два', '3', 'три', '4', 'чотири'],
                                 'Введіть 1, 2, 3 або 4')
-            column_name = 'A'
             file_name = 'districts'
             param = 'Берег Дніпра'
             if way == '1' or way == 'один':
                 element = Element(param, file_name, 'G')
-                param = element.define_element()
-            if way == '2' or way == 'два':
-                element = Element('Район', file_name, column_name)
-                param = element.define_element()
-            if way == '3' or way == 'три':
-                pass
+                param = element.define_param()
+            elif way == '2' or way == 'два':
+                element = Element('Район', file_name)
+                param = element.define_param()
+            # elif way == '3' or way == 'три':
                 # element = Element('Місцевість у районі', 'streets', 'E')
                 # param = element.define_element()
-            if way == '4' or way == 'чотири':
-                element = Element('Район', file_name, column_name)
-                district = element.define_element()
-                if district == ' не знайдено':
-                    continue
-                element = Element('Вулицю', 'streets', column_name, district, 'B')
-                param = element.define_element()
+            else:
+                element = Element('Вулицю', 'streets')
+                param = element.define_param()
             if param == ' не знайдено':
                 continue
 
@@ -45,11 +39,10 @@ def main():
             program.output(messages.objects_nearby)
             way = program.input(['1', 'один', '2', 'два', '3', 'три', '4', 'чотири', '5', "п'ять"],
                                 'Введіть 1, 2, 3, 4 або 5')
-            found_obj = ''
+            objs_nearby = []
             if way == '1' or way == 'один':
-                pass
-                # element = Element(found_obj, file_name, column_name)
-                # found_obj = element.define_element()
+                objs_nearby = element.define_obj_nearby(param, 'food_stores/atb')
+                print(objs_nearby)
             if way == '2' or way == 'два':
                 pass
             if way == '3' or way == 'три':
